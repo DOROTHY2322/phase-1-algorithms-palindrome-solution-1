74afe5c26a403b8568ecd4cbd661d7acdc29bd94
DOROTHY2322 <kimelidorothy@gmail.com>
2022-12-07 20:02:46 +0300

    solution

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function isPalindrome(word) {
   // Write your algorithm here
   //create an array for the reversed string
-    let polishedString = word.replace();
-    //reverse the array
-    let reversedstring = polishedString.split("").reverse().join("")
-    //create a string from the reversed array
-    console.log(reversedstring, polishedString)
-    if(polishedString != reversedstring){
-      return false;
-}
-return true;
+  let polishedString = word.replace();
+  //reverse the array
+  let reversedstring = polishedString.split("").reverse().join("")
+  //create a string from the reversed array
+  console.log(reversedstring, polishedString)
+  if (polishedString != reversedstring) {
+    return false;
+  }
+  return true;
 }
 let result = isPalindrome("abba")
 console.log(result)
@@ -21,6 +21,14 @@ console.log(result)
 /*
   Add written explanation of your solution here
 */
+//in our algorithm, the reverseString
+//Split the string
+// That takes up more memory to store the new array, as well as the time it takes JavaScript to iterate over each character of the string to produce a new array
+// Reverse the array
+// That takes up more memory to store the reversed array, as well as the time it takes JavaScript to iterate over the array to produce a reversed array
+// Join the string
+// That takes up more memory to store the new string, as well as the time it takes JavaScript to iterate over the array to produce a string
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
